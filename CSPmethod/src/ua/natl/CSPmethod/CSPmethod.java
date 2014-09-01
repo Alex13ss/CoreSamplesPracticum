3aa9413561fb07d82bb87de9ee044c8c2569af21
Олександр <newbeecon@mail.ru>
2014-09-01 14:11:48 +0300

    Sample of a method (simple)

---
+++ CSPmethod/src/ua/natl/CSPmethod/CSPmethod.java
@@ -0,0 +1,18 @@
+package ua.natl.CSPmethod;
+
+public class CSPmethod {
+
+	public static void main(String[] args) {
+		int i = 3;
+		int k = 5;
+		i = count(i);
+		k = count(k);
+		System.out.println(i);
+		System.out.println(k);
+	}
+	
+	private static int count(int x){
+		x = x + 1;
+		return x;
+	}
+}
