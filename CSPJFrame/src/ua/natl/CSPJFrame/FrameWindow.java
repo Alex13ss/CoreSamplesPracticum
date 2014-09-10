38a0a83831fc997d9708688e1d0c1eeefc63bbc5
Олександр <newbeecon@mail.ru>
2014-09-10 16:50:54 +0300

    Added ActionListener with try{}catch{} statement.

--- CSPJFrame/src/ua/natl/CSPJFrame/FrameWindow.java
+++ CSPJFrame/src/ua/natl/CSPJFrame/FrameWindow.java
@@ -11,6 +11,7 @@ public class FrameWindow extends JFrame {
 	JTextField t1, t2;
 	int i, k;
 	String a, b;
+	eHandler hanler = new eHandler();
 	
 	public FrameWindow(String s){
 		super(s);
@@ -31,5 +32,31 @@ public class FrameWindow extends JFrame {
 		add(t2);
 		add(l3);
 		add(l4);
+		b2.addActionListener(hanler);
+		b1.addActionListener(hanler);
+	}
+	
+	public class eHandler implements ActionListener {
+		public void actionPerformed(ActionEvent ev){
+			try{
+				if(ev.getSource()==b2){
+					i = Integer.parseInt(t1.getText());
+					k = Integer.parseInt(t2.getText());
+					i *= 100;
+					k *= 100;
+					a = "Now Your first number = " + i;
+					b = "Now Your second number = " + k;
+					l3.setText(a);
+					l4.setText(b);
+				}
+				
+				if(ev.getSource()==b1){
+					t1.setText(null);
+					t2.setText(null);
+					l3.setText("");
+					l4.setText("");
+				}
+			}catch(Exception e){JOptionPane.showMessageDialog(null, "Enter a NUMBER!");}	
+		}
 	}
 }
