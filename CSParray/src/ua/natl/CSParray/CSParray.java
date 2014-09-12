b1a1ec6699b5b418104c649f1349ef0a191598d5
Олександр <newbeecon@mail.ru>
2014-09-12 10:10:58 +0300

    Sample of simple array

---
+++ CSParray/src/ua/natl/CSParray/CSParray.java
@@ -0,0 +1,15 @@
+package ua.natl.CSParray;
+
+public class CSParray {
+
+	public static void main(String[] args) {
+		
+		int[] m = new int[5];
+		
+		for(int i=0;i<5;i++){
+			m[i] = i*-1;
+			System.out.println(m[i]);
+		}
+		
+	}
+}
