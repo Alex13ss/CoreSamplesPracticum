5f1f11bb4bdef00616a74ea14dd7bb4ecde951b4
Олександр <newbeecon@mail.ru>
2014-09-12 12:36:31 +0300

    Sample of multidimensional array

---
+++ CSPmultArray/src/ua/natl/CSPmultArray/MultiArray.java
@@ -0,0 +1,22 @@
+package ua.natl.CSPmultArray;
+
+public class MultiArray {
+
+	public static void main(String[] args) {
+	
+		//Initialization of two dimensional array without auto filling:
+		//int[][] m = {{1,2,3},{9,5,1},{4,7,2}};
+		
+		int[][] m = new int[3][3];
+		for(int row=0;row<m.length;row++){
+			for(int col=0;col<m[row].length;col++){
+				m[row][col] = row + col;
+				System.out.print(m[row][col] + "  ");
+				try{
+					Thread.sleep(1000);
+				}catch(Exception e){}
+			}
+		System.out.println();
+		}
+	}
+}
