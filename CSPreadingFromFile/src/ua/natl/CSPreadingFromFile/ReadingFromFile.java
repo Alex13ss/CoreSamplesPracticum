3f2dd19abce5d7edcea00b0adf36b7e3b1b4a7fe
Alex <newbeecon@mail.ru>
2014-09-22 22:25:37 +0300

    Let's create some snippet, which use array for reading smth from file.

---
+++ CSPreadingFromFile/src/ua/natl/CSPreadingFromFile/ReadingFromFile.java
@@ -0,0 +1,29 @@
+package ua.natl.CSPreadingFromFile;
+
+public class ReadingFromFile {
+	String[][] m = new String[5][3];
+
+	public static void main(String[] args) {
+		
+		openFile();
+		readFile();
+		out();
+
+	}
+
+	private static void out() {
+		
+		
+	}
+
+	private static void readFile() {
+		
+		
+	}
+
+	private static void openFile() {
+		
+		
+	}
+
+}
