9071e759289a47f41781e54ab83340ca2bc5b2f4
Alex <newbeecon@mail.ru>
2014-09-27 21:54:05 +0300

    Sample of Collections (ArrayList) use

--- CSPCollections/src/ua/natl/CSPCollections/Main.java
+++ CSPCollections/src/ua/natl/CSPCollections/Main.java
@@ -1,8 +1,18 @@
 package ua.natl.CSPCollections;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
+	private static ArrayList<Mobiles> mobiles = new ArrayList<Mobiles>();
+	private static Random r = new Random();
 
 	public static void main(String[] args) {
-		
+		for(int i=0;i<300;i++) {
+			mobiles.add(new Mobiles(r.nextInt(999999999), "PhoneName"));
+		}
+		for(Mobiles m : mobiles) {
+			System.out.println(m.getNumber() + " " + m.getName());
+		}
 	}
 }
