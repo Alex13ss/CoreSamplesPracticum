6c2f49a9798474767640b006dc8cd1ee51395847
Alex <newbeecon@mail.ru>
2014-10-05 14:31:45 +0300

    Sample of Inner and Nested Classes

---
+++ CSPinnerNested/src/ua/natl/innernested/Main.java
@@ -0,0 +1,18 @@
+package ua.natl.innernested;
+
+import ua.natl.CSPinnerNested.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyTest test = new MyTest();
+		test.show();
+		
+		MyTest.MyNested nested = new MyTest.MyNested();
+		nested.nestedShow();
+		
+		MyTest.MyInner inner = test.new MyInner();
+		inner.innerShow();
+		
+	}
+}
