cb68c1d7cb8f4bc0a60b7b417fb8589c4ddd04dc
Alex <newbeecon@mail.ru>
2014-10-12 01:50:44 +0300

    Sample of FileInputStream & FileOutputStream use

--- CSPfileio/src/ua/natl/frfw/FrFwMain.java
+++ CSPfileio/src/ua/natl/frfw/FrFwMain.java
@@ -1,16 +1,25 @@
 package ua.natl.frfw;
 
-import java.io.*;
-
-import static java.lang.System.out;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FrFwMain {
 
 	public static void main(String[] args) throws IOException {
 		
 		FileInputStream fs = new FileInputStream("test.txt");
-		
+		while (fs.available() != 0) {
+			int b = fs.read();
+			char ch = (char)b;
+			System.out.print(ch);
+		}
 		fs.close();
-
+		
+		FileOutputStream fo = new FileOutputStream("out.txt");
+		String s = "Hello";
+		byte[] bytes = s.getBytes();
+		fo.write(bytes);
+		fo.close();
 	}
 }
