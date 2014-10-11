da83e9d8e05d2fc647c37ac95149440e4c939f47
Alex <newbeecon@mail.ru>
2014-10-11 23:53:51 +0300

    Init

--- CSPfrfw/src/ua/natl/frfw/FrFwMain.java
+++ CSPfrfw/src/ua/natl/frfw/FrFwMain.java
@@ -1,13 +1,16 @@
 package ua.natl.frfw;
 
 import java.io.*;
+
 import static java.lang.System.out;
 
 public class FrFwMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		FileInputStream fs = new FileInputStream("test.txt");
 		
+		fs.close();
 
 	}
 }
