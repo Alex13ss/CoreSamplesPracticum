858ff26f9f21862005b5a2be121a2d474f6fcb6b
Alex <newbeecon@mail.ru>
2014-10-13 21:52:09 +0300

    + FileReader & PrintWriter

--- CSPfileio/src/ua/natl/frfw/FrFwMain.java
+++ CSPfileio/src/ua/natl/frfw/FrFwMain.java
@@ -1,8 +1,11 @@
 package ua.natl.frfw;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FrFwMain {
 
@@ -21,5 +24,16 @@ public class FrFwMain {
 		byte[] bytes = s.getBytes();
 		fo.write(bytes);
 		fo.close();
+		
+		BufferedReader reader = new BufferedReader (new FileReader("test.txt"));
+		System.out.println("\n*************");
+		while ( (s = reader.readLine()) != null) {
+			System.out.println(s);
+		}
+		reader.close();
+		
+		PrintWriter writer = new PrintWriter("test2.txt");
+		writer.printf("%s %d %c", "String", 12345, 'G');
+		writer.close();
 	}
 }
