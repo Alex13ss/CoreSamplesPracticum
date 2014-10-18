4676f986008a85c8a9179c08e8b34689a0919e76
Alex <newbeecon@mail.ru>
2014-10-18 23:05:35 +0300

    + random filling

--- CSParray/src/ua/natl/CSParray/CSParray.java
+++ CSParray/src/ua/natl/CSParray/CSParray.java
@@ -1,13 +1,17 @@
 package ua.natl.CSParray;
 
+import java.util.Random;
+
 public class CSParray {
 
 	public static void main(String[] args) {
 		
+		Random r = new Random();
+		
 		int[] m = new int[5];
 		
-		for(int i=0;i<5;i++){
-			m[i] = i*-1;
+		for(int i=0;i<m.length;i++){
+			m[i] = r.nextInt(777);
 			System.out.println(m[i]);
 		}
 		
