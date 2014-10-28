3111cb51c13ddb416a7dfd5a29757ed4da009bcb
Alex <newbeecon@mail.ru>
2014-10-28 23:03:28 +0200

    sample

---
+++ CSPJFrame/src/ua/natl/jframe/MainFrame.java
@@ -0,0 +1,59 @@
+package ua.natl.jframe;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class MainFrame extends JFrame {
+
+	/**
+	 * Created by Alex on October 28, 2014 (22:00)
+	 * All rights are reserved.
+	 * xD
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+
+		new MainFrame();
+
+	}
+
+	public MainFrame() throws HeadlessException {
+		super();
+		
+		setTitle("Hello");
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setBackground(Color.GREEN);
+		//setPreferredSize(new Dimension(500, 400));
+		setSize(500, 400);
+		setLocationRelativeTo(null);
+		addComponents(getContentPane());
+		//pack();
+		setVisible(true);
+	}
+
+	private void addComponents(Container contentPane) {
+		contentPane.setLayout(new BorderLayout());
+		
+		JPanel p = new JPanel();
+		p.setBackground(Color.BLUE);
+		
+		JButton b = new JButton();
+		b.setBackground(Color.YELLOW);
+		b.setText("Go-go-go");
+		
+		contentPane.add(p, BorderLayout.NORTH);
+		contentPane.add(b, BorderLayout.WEST);
+		contentPane.add(new JTextField("Hello"), BorderLayout.SOUTH);
+		contentPane.add(new JTextArea("JJJJJ"), BorderLayout.CENTER);
+		
+	}
+}
