5e3a2b1262bc883858441ab82ae7e59a842f165e
Alex <newbeecon@mail.ru>
2014-11-05 22:14:40 +0200

    less code rows

--- MyCalculator/src/ua/natl/mycalculator/Calculator.java
+++ MyCalculator/src/ua/natl/mycalculator/Calculator.java
@@ -1,24 +1,20 @@
 package ua.natl.mycalculator;
 
-import javax.swing.*;
-
-import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Calculator {
 
 	JPanel windowContent;
-	JTextField displayField;
-	JButton button0;
-	JButton button1;
-	JButton button2;
-	JButton button3;
-	JButton button4;
-	JButton button5;
-	JButton button6;
-	JButton button7;
-	JButton button8;
-	JButton button9;
+	JFormattedTextField displayField;
+	JButton[] button = new JButton[10];
 	JButton buttonPoint;
 	JButton buttonEqual;
 	JPanel p1;
@@ -28,35 +24,25 @@ public class Calculator {
 		windowContent = new JPanel();
 		BorderLayout bl = new BorderLayout();
 		windowContent.setLayout(bl);
-		displayField = new JTextField(30);
+		displayField = new JFormattedTextField();
+		displayField.setPreferredSize(new Dimension(250, 25));
+		displayField.setHorizontalAlignment(SwingConstants.RIGHT);
 		windowContent.add("North", displayField);
-		
-		button0 = new JButton("0");
-		button1 = new JButton("1");
-		button2 = new JButton("2");
-		button3 = new JButton("3");
-		button4 = new JButton("4");
-		button5 = new JButton("5");
-		button6 = new JButton("6");
-		button7 = new JButton("7");
-		button8 = new JButton("8");
-		button9 = new JButton("9");
+		for (int i = 0; i < 10; i++) {
+			button[i] = new JButton("" + i);
+		}
 		buttonPoint = new JButton(".");
 		buttonEqual = new JButton("=");
 		
 		p1 = new JPanel();
 		GridLayout gl = new GridLayout(4, 3);
 		p1.setLayout(gl);
-		p1.add(button1);
-		p1.add(button2);
-		p1.add(button3);
-		p1.add(button4);
-		p1.add(button5);
-		p1.add(button6);
-		p1.add(button7);
-		p1.add(button8);
-		p1.add(button9);
-		p1.add(button0);
+		for (int i = 1; i <= 10; i++)
+			if (i == 10) {
+				p1.add(button[0]);
+			} else {
+				p1.add(button[i]);
+			}
 		p1.add(buttonPoint);
 		p1.add(buttonEqual);
 		windowContent.add("Center", p1);
